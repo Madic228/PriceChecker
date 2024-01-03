c02e7abf5b209156461d7c24ea7dabde75b2a416
cream <nekost9@mail.ru>
2024-01-03 17:32:04 +0500

    commands added

--- ProjectFiles/main.py
+++ ProjectFiles/main.py
@@ -5,7 +5,28 @@ bot = telebot.TeleBot("6798262829:AAGlgaecBZRfuJOTckQhbfbZzRFi1KxB8_Y")
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, "Привет")
+    bot.send_message(message.chat.id, "start")
+
+@bot.message_handler(commands=['goodlist'])
+def start(message):
+    bot.send_message(message.chat.id, "goodlist")
+
+@bot.message_handler(commands=['addgood'])
+def start(message):
+    bot.send_message(message.chat.id, "addgood")
+
+@bot.message_handler(commands=['deletegood'])
+def start(message):
+    bot.send_message(message.chat.id, "deletegood")
+
+@bot.message_handler(commands=['deleteall'])
+def start(message):
+    bot.send_message(message.chat.id, "deleteall")
+
+@bot.message_handler()
+def free_text(message):
+    bot.send_message(message.chat.id, 'выберите команду')
+
 
 
 bot.infinity_polling()
