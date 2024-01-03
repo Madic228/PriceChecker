f8dfb173b2c66758f043f25068265bd2a170f454
cream <nekost9@mail.ru>
2024-01-03 17:07:06 +0500

    hello message

--- ProjectFiles/main.py
+++ ProjectFiles/main.py
@@ -5,3 +5,4 @@ bot = telebot.TeleBot("6798262829:AAGlgaecBZRfuJOTckQhbfbZzRFi1KxB8_Y")
 
 @bot.message_handler(commands=['start'])
 def start(message):
+    bot.send_message(message.chat_id, "Привет")
